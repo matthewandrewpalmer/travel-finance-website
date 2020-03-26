5d9a8ca9f148ae909a1689d5ee4cec7df9a544ed
Matthew Palmer <matthewandrewpalmer@outlook.com>
2020-03-26 22:21:39 +0000

    JourneyForm.js

--- src/components/JourneyForm.js
+++ src/components/JourneyForm.js
@@ -14,6 +14,14 @@ import KeyboardReturnIcon from '@material-ui/icons/KeyboardReturn';
 import PublicIcon from '@material-ui/icons/Public';
 import DirectionsBusIcon from '@material-ui/icons/DirectionsBus';
 import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import FormControl from "@material-ui/core/FormControl";
+import ButtonBase from "@material-ui/core/ButtonBase";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Switch from "@material-ui/core/Switch";
 
 
 const useStyles = makeStyles(theme => ({
@@ -22,9 +30,24 @@ const useStyles = makeStyles(theme => ({
     },
     staticList: {
         height: "100vh",
-        overflow: "auto",
+        overFlow: "auto",
         // background: "#3D3861"
     },
+    margin: {
+        margin: theme.spacing(1),
+    },
+    journeyTypeList: {
+        marginLeft: "auto",
+        marginRight: "auto",
+    },
+    journeyType: {
+        borderRadius: "50%",
+        height: "70px",
+        width: "70px",
+        textAlign: "center",
+        alignItems: "center",
+        marginLeft: "20px"
+    }
 }));
 
 
@@ -39,89 +62,144 @@ function JourneyForm() {
         setName(event.target.value);
     };
 
+    // Form items State
+    const [journeyType, setJourneyType] = React.useState('Single');
     const [departing, setDeparting] = React.useState();
     const [destination, setDestination] = React.useState();
-
     const [date, setDate] = useState(new Date());
+    const [cost, setCost] = React.useState();
+    const [railcardUsed, setRailcardUsed] = React.useState(false);
 
     const MaterialInput = ({value, onClick}) => (
         <TextField id="standard-basic" label="Date" value={value} onClick={onClick}/>
     );
 
-    const [journeyType, setJourneyType] = React.useState('Single');
-
     return (
         <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <h1>New Journey</h1>
+            <Container maxWidth="sm">
+                <h1>New Journey</h1>
 
-            <form className={styles.root} noValidate autoComplete="off">
-                <div style={{alignItems: "center"}}>
-                    <div onClick={() => setJourneyType('Single')}
-                         style={{float: "left", marginLeft: "10px", alignItems: "center"}}>
-                        <ArrowForwardIcon value={'Single'} color={journeyType === 'Single' ? "primary" : "disabled"}/>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            Single
-                        </Typography>
+                <form className={styles.root} noValidate autoComplete="off">
+                    <div className={styles.journeyTypeList}>
+                        <ButtonBase
+                            focusRipple
+                            className={styles.journeyType}
+                            onClick={() => setJourneyType('Single')}
+                        >
+                            <div>
+                                <ArrowForwardIcon value={'Single'}
+                                                  color={journeyType === 'Single' ? "primary" : "disabled"}/>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    Single
+                                </Typography>
+                            </div>
+                        </ButtonBase>
+                        <ButtonBase
+                            focusRipple
+                            className={styles.journeyType}
+                            onClick={() => setJourneyType('Return')}
+                        >
+                            <div>
+                                <KeyboardReturnIcon value={'Return'}
+                                                    color={journeyType === 'Return' ? "primary" : "disabled"}/>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    Return
+                                </Typography>
+                            </div>
+                        </ButtonBase>
+                        <ButtonBase
+                            focusRipple
+                            className={styles.journeyType}
+                            onClick={() => setJourneyType('Rover')}
+                        >
+                            <div>
+                                <PublicIcon value={'Rover'} color={journeyType === 'Rover' ? "primary" : "disabled"}/>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    Rover
+                                </Typography>
+                            </div>
+                        </ButtonBase>
+                        <ButtonBase
+                            focusRipple
+                            className={styles.journeyType}
+                            onClick={() => setJourneyType('PlusBus')}
+                        >
+                            <div>
+                                <DirectionsBusIcon value={'PlusBus'}
+                                                   color={journeyType === 'PlusBus' ? "primary" : "disabled"}/>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    Plus Bus
+                                </Typography>
+                            </div>
+                        </ButtonBase>
                     </div>
-                    <div onClick={() => setJourneyType('Return')}
-                         style={{float: "left", marginLeft: "10px", alignItems: "center"}}>
-                        <KeyboardReturnIcon value={'Return'} color={journeyType === 'Return' ? "primary" : "disabled"}/>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            Return
-                        </Typography>
-                    </div>
-                    <div onClick={() => setJourneyType('Rover')}
-                         style={{float: "left", marginLeft: "10px", alignItems: "center"}}>
-                        <PublicIcon value={'Rover'} color={journeyType === 'Rover' ? "primary" : "disabled"}/>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            Rover
-                        </Typography>
-                    </div>
-                    <div onClick={() => setJourneyType('PlusBus')}
-                         style={{float: "left", marginLeft: "10px", alignItems: "center"}}>
-                        <DirectionsBusIcon value={'PlusBus'}
-                                           color={journeyType === 'PlusBus' ? "primary" : "disabled"}/>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            Plus Bus
-                        </Typography>
-                    </div>
-                </div>
 
+                    <FormControl fullWidth className={styles.margin}>
+                        <Autocomplete
+                            options={stationList}
+                            getOptionLabel={station => station.name + " (" + station.code + ")"}
+                            id="departing-station-field"
+                            autoHighlight
+                            value={departing}
+                            onChange={(event, newValue) => {
+                                setDeparting(newValue);
+                            }}
+                            renderInput={params => (
+                                <TextField {...params} label="Departing Station" margin="normal" fullWidth/>
+                            )}
+                        />
+                    </FormControl>
+                    <FormControl fullWidth className={styles.margin}>
+                        <Autocomplete
+                            options={stationList}
+                            getOptionLabel={station => station.name + " (" + station.code + ")"}
+                            id="destination-station-field"
+                            autoHighlight
+                            value={destination}
+                            onChange={(event, newValue) => {
+                                setDestination(newValue);
+                            }}
+                            renderInput={params => (
+                                <TextField {...params} label="Destination Station" margin="normal" fullWidth/>
+                            )}
+                        />
+                    </FormControl>
+                    <FormControl fullWidth className={styles.margin}>
+                        <DatePicker
+                            selected={date}
+                            onChange={date => setDate(date)}
+                            customInput={<MaterialInput/>}
+                        />
+                    </FormControl>
 
-                <Autocomplete
-                    options={stationList}
-                    getOptionLabel={station => station.name + " (" + station.code + ")"}
-                    id="departing-station-field"
-                    autoHighlight
+                    <FormControlLabel
+                        control={
+                            <Switch
+                                checked={railcardUsed}
+                                onChange={(event, newValue) => {
+                                    setRailcardUsed(newValue);
+                                }}
+                                name="railcardUsed"
+                                color="primary"
+                            />
+                        }
+                        label="Railcard used"
+                    />
 
-                    value={departing}
-                    onChange={(event, newValue) => {
-                        setDeparting(newValue);
-                    }}
-                    renderInput={params => (
-                        <TextField {...params} label="Departing Station" margin="normal" fullWidth/>
-                    )}
-                />
-                <Autocomplete
-                    options={stationList}
-                    getOptionLabel={station => station.name + " (" + station.code + ")"}
-                    id="destination-station-field"
-                    autoHighlight
-                    value={destination}
-                    onChange={(event, newValue) => {
-                        setDestination(newValue);
-                    }}
-                    renderInput={params => (
-                        <TextField {...params} label="Destination Station" margin="normal" fullWidth/>
-                    )}
-                />
-                <DatePicker
-                    selected={date}
-                    onChange={date => setDate(date)}
-                    customInput={<MaterialInput/>}
-                />
-                <TextField id="standard-basic" label="Departing" value={name} onChange={handleChange}/>
-            </form>
+                    <FormControl fullWidth className={styles.margin}>
+                        <InputLabel htmlFor="standard-adornment-amount">Total Cost</InputLabel>
+                        <Input
+                            id="standard-adornment-amount"
+                            value={cost}
+                            onChange={(event, newValue) => {
+                                setCost(newValue);
+                            }}
+                            startAdornment={<InputAdornment position="start">£</InputAdornment>}
+                        />
+                    </FormControl>
+                    {/*<TextField id="standard-basic" label="Departing" value={name} onChange={handleChange}/>*/}
+                </form>
+            </Container>
         </MuiPickersUtilsProvider>
     );
 }
